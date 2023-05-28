ce9a2eea1832829b8d8df43939a959525c7a3022
raul effting <rauleffting@rauls-MBP.lan>
2023-05-28 18:22:32 -0300

    add header layout

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,14 +1,28 @@
 'use client'
 
-import { List } from '@phosphor-icons/react'
+import { List, MagnifyingGlass } from '@phosphor-icons/react'
 
 export function Header() {
   return (
-    <header className="flex justify-between items-center bg-stone-950">
-      <h4>BookShelter</h4>
-      <input type="text" placeholder="Search" />
-      <nav>
-        <List />
+    <header className="flex justify-between items-center h-16 px-2 bg-stone-950">
+      <h4 className="text-base text-amber-400 pr-2">
+        <span className="text-white">Book</span>
+        Shelter
+      </h4>
+      <div className="flex items-center">
+        <input
+          type="text"
+          placeholder="Search"
+          className="flex pl-2 h-6 rounded-tl-xl rounded-bl-xl text-stone-950 focus:outline-none"
+        />
+        <div className="flex h-6 p-2 items-center justify-center rounded-tr-xl rounded-br-xl bg-amber-400 cursor-pointer hover:bg-amber-500">
+          <MagnifyingGlass size={20} className="text-stone-950" />
+        </div>
+      </div>
+      <nav className="flex items-center">
+        <button>
+          <List size={20} />
+        </button>
       </nav>
     </header>
   )
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,18 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body>
         <Header />
         <main className="h-screen px-4 bg-stone-200">{children}</main>
