49c3d53482955782ddb50f5dded93a13b909fc80
Raul Effting <rauleffting94@gmail.com>
2023-06-01 18:09:39 -0300

    add dropdownmenu

---
+++ src/app/components/dropdownMenu.tsx
@@ -0,0 +1,9 @@
+export function DropdownMenu() {
+  return (
+    <div className="absolute top-16 right-0 bg-stone-950 transition-opacity duration-1000 opacity-100">
+      <p className="hover:text-amber-400">Favorites</p>
+      <p className="hover:text-amber-400">Login</p>
+      <p className="hover:text-amber-400">Cart</p>
+    </div>
+  )
+}
--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,10 +1,16 @@
-'use client'
-
 import { List, MagnifyingGlass } from '@phosphor-icons/react'
+import { useState } from 'react'
+import { DropdownMenu } from './dropdownMenu'
 
 export function Header() {
+  const [toggle, setToggle] = useState<boolean>(false)
+
+  function handleDropdownMenu() {
+    setToggle(!toggle)
+  }
+
   return (
-    <header className="flex justify-between items-center h-16 px-2 bg-stone-950">
+    <header className="flex justify-between items-center h-16 px-2 bg-stone-950 relative">
       <h4 className="text-base text-amber-400 pr-2">
         <span className="text-white">Book</span>
         Shelter
@@ -13,17 +19,18 @@ export function Header() {
         <input
           type="text"
           placeholder="Search"
-          className="flex pl-2 h-6 rounded-tl-xl rounded-bl-xl text-stone-950 focus:outline-none"
+          className="flex pl-2 h-6 rounded-tl-xl rounded-bl-xl text-stone-950 focus:outline-none focus:ring-2 focus:ring-amber-400 focus:h-5"
         />
         <div className="flex h-6 p-2 items-center justify-center rounded-tr-xl rounded-br-xl bg-amber-400 cursor-pointer hover:bg-amber-500">
           <MagnifyingGlass size={20} className="text-stone-950" />
         </div>
       </div>
-      <nav className="flex items-center">
-        <button>
-          <List size={20} />
-        </button>
-      </nav>
+
+      <button onClick={handleDropdownMenu}>
+        <List size={20} className="text-stone-100 hover:text-stone-400" />
+      </button>
+
+      {toggle && <DropdownMenu />}
     </header>
   )
 }
