5aa7013915c1ed0cbadb79893e629316c26571f7
Raul Effting <rauleffting94@gmail.com>
2023-06-06 17:51:36 -0300

    finish mobile header

--- src/app/components/dropdownMenu.tsx
+++ src/app/components/dropdownMenu.tsx
@@ -1,9 +1,20 @@
+import { ShoppingCart, Star, User } from '@phosphor-icons/react'
+
 export function DropdownMenu() {
   return (
-    <div className="absolute top-16 right-0 bg-stone-950 transition-opacity duration-1000 opacity-100">
-      <p className="hover:text-amber-400">Favorites</p>
-      <p className="hover:text-amber-400">Login</p>
-      <p className="hover:text-amber-400">Cart</p>
+    <div className="absolute top-16 right-0 h-36 w-36 p-4 flex flex-col justify-between rounded-bl-lg bg-stone-100 text-stone-950  shadow-md">
+      <div className="flex gap-4 hover:cursor-pointer hover:scale-105 transition-transform">
+        <ShoppingCart size={20} />
+        <span>Cart</span>
+      </div>
+      <div className="flex gap-4 hover:cursor-pointer hover:scale-105 transition-transform">
+        <Star size={20} />
+        <span>Favorites</span>
+      </div>
+      <div className="flex gap-4 hover:cursor-pointer hover:scale-105 transition-transform">
+        <User size={20} />
+        <span>Login</span>
+      </div>
     </div>
   )
 }
--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,27 +1,46 @@
 import { List, MagnifyingGlass } from '@phosphor-icons/react'
-import { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { DropdownMenu } from './dropdownMenu'
 
 export function Header() {
   const [toggle, setToggle] = useState<boolean>(false)
+  const dropdownRef = useRef<HTMLDivElement>(null)
 
-  function handleDropdownMenu() {
+  function handleDropdownMenu(event: React.MouseEvent<HTMLButtonElement>) {
+    event.stopPropagation() // Stop event propagation
     setToggle(!toggle)
   }
 
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target as Node)
+      ) {
+        setToggle(false)
+      }
+    }
+
+    window.addEventListener('click', handleClickOutside)
+
+    return () => {
+      window.removeEventListener('click', handleClickOutside)
+    }
+  }, [])
+
   return (
     <header className="flex justify-between items-center h-16 px-2 bg-stone-950 relative">
-      <h4 className="text-base text-amber-400 pr-2">
+      <h4 className="flex text-base text-amber-400 pr-2">
         <span className="text-white">Book</span>
         Shelter
       </h4>
-      <div className="flex items-center">
+      <div className="flex items-center justify-center flex-grow">
         <input
           type="text"
           placeholder="Search"
-          className="flex pl-2 h-6 rounded-tl-xl rounded-bl-xl text-stone-950 focus:outline-none focus:ring-2 focus:ring-amber-400 focus:h-5"
+          className="flex pl-2 h-6 rounded-tl-md rounded-bl-md text-stone-950 focus:outline-none focus:ring-2 focus:ring-amber-400 focus:h-5"
         />
-        <div className="flex h-6 p-2 items-center justify-center rounded-tr-xl rounded-br-xl bg-amber-400 cursor-pointer hover:bg-amber-500">
+        <div className="flex h-6 p-2 items-center justify-center rounded-tr-md rounded-br-md bg-amber-400 cursor-pointer hover:bg-amber-500">
           <MagnifyingGlass size={20} className="text-stone-950" />
         </div>
       </div>
@@ -30,7 +49,14 @@ export function Header() {
         <List size={20} className="text-stone-100 hover:text-stone-400" />
       </button>
 
-      {toggle && <DropdownMenu />}
+      <div
+        ref={dropdownRef}
+        className={`transition-opacity duration-500 ${
+          toggle ? 'opacity-100 visible' : 'opacity-0 invisible'
+        }`}
+      >
+        <DropdownMenu />
+      </div>
     </header>
   )
 }
