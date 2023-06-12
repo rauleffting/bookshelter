a9a2d50096d07c70604816600e8130bbdef5efe4
Raul Effting <rauleffting94@gmail.com>
2023-06-12 17:02:10 -0300

    add footer

---
+++ src/app/components/footer.tsx
@@ -0,0 +1,13 @@
+export function Footer() {
+  return (
+    <footer className="flex flex-col items-center h-24 my-4 mb-20 p-4">
+      <div className="flex mb-4 text-amber-600">
+        <p className="mr-8 hover:font-bold hover:cursor-pointer w-16">About</p>
+        <p className="hover:font-bold hover:cursor-pointer w-16">Contact</p>
+      </div>
+      <p className="text-stone-400">
+        <strong>© 2023 BookShelter.</strong> All rights reserved
+      </p>
+    </footer>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { Cover } from './components/cover'
+import { Footer } from './components/footer'
 import { Section } from './components/section'
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
     <div className="h-screen p-2 bg-stone-200 overflow-auto">
       <Cover />
       <Section />
+      <Footer />
     </div>
   )
 }
